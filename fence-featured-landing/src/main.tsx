43eb06f5672691fcd517a5b8ac06ff0774716c0e
Evan Alexander Adamson <evan.alexander.adamson@gmail.com>
2025-01-29 13:34:01 -0600

    chore: update base URL and router basename for new repository

--- fence-featured-landing/src/main.tsx
+++ fence-featured-landing/src/main.tsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter basename="/Scroggs-Fencing">
+    <BrowserRouter basename="/Homepage">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
